39c0ee47beba346011e2b90ae5b58bbe2d9e7bad
LuaxY <yann.guineau@voidmx.net>
2014-05-24 12:15:50 +0200

    ajout de commentaires (FR) sur QKinect et DrawZone

--- drawzone.cpp
+++ drawzone.cpp
@@ -4,6 +4,11 @@
 
 #include <QDebug>
 
+/**
+ * @brief DrawZone::DrawZone : constructeur
+ * @param parent : classe parrent
+ * @param main : IHM
+ */
 DrawZone::DrawZone(QWidget *parent, QWidget *main) :
     QWidget(parent),
     mMain(main)
@@ -15,14 +20,19 @@ DrawZone::DrawZone(QWidget *parent, QWidget *main) :
         skeleton[i].untracked = 0;
     }
 
+    // Affectation d'un couleur différente pour chaque squelette
     skeleton[0].color = Qt::blue;
     skeleton[1].color = Qt::magenta;
     skeleton[2].color = Qt::cyan;
     skeleton[3].color = Qt::yellow;
 }
 
+/**
+ * @brief DrawZone::paintEvent : fonction event qui permet de dessiner
+ */
 void DrawZone::paintEvent(QPaintEvent*)
 {
+    // Préparation de la zone de dessin
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing, true);
     painter.fillRect(QRect(0, 0, width(), height()), Qt::black);
@@ -46,15 +56,17 @@ void DrawZone::paintEvent(QPaintEvent*)
     hightlight(painter, mouse.x(), mouse.y());
     painter.setBrush(Qt::NoBrush);
 
-    /* DRAW SKELETON */
+    /* AFFICHAGE DES SQUELETTES */
 
-    lock();
+    lock(); // mutex pour bloquer le vecteur de données des squelettes (thread safe)
 
     for(unsigned int i = 0; i < 5; i++)
     {
+        // Si le squelettes n'a pas encore de données on passe
         if(!skeleton[i].isSet)
             continue;
 
+        // Si le squelettes n'est plus traquer depuis 20 frames on le supprime
         if(skeleton[i].untracked > 0)
         {
             skeleton[i].untracked++;
@@ -65,32 +77,41 @@ void DrawZone::paintEvent(QPaintEvent*)
             }
         }
 
+        // On dessine chaque jointure du squelette
         for(unsigned int j = 0; j < 19; j++)
         {
             Jointure joint = skeleton[i].jointures[j];
 
+            // Si la position de l'un des deux point est inconnue, on passe
             if(skeleton[i].data.eSkeletonPositionTrackingState[joint.pos1] == NUI_SKELETON_NOT_TRACKED || skeleton[i].data.eSkeletonPositionTrackingState[joint.pos2] == NUI_SKELETON_NOT_TRACKED)
                 continue;
 
+            // Ajustement des coordonnées en fonction de la zone de dessin
             QPoint pos1 = adjustSize(skeleton[i].data.SkeletonPositions[joint.pos1]);
             QPoint pos2 = adjustSize(skeleton[i].data.SkeletonPositions[joint.pos2]);
 
+            // Si la jointures est traquer (TRACKED) on dessine la jointure avec la couleur respectif du squelette
+            // Si c'est pas le cas (INFERRED) on dessine la jointure en gris
             if(skeleton[i].data.eSkeletonPositionTrackingState[joint.pos1] == NUI_SKELETON_POSITION_TRACKED || skeleton[i].data.eSkeletonPositionTrackingState[joint.pos2] == NUI_SKELETON_POSITION_TRACKED)
                 painter.setPen(QPen(skeleton[i].color, 3));
             else
                 painter.setPen(QPen(Qt::gray, 1));
 
+            // Ligne de la jointure
             painter.drawLine(pos1, pos2);
 
+            // Même chose pour les extrémitée de la jointure, TRACKED = vert, INFERRED = vert foncé
             if(skeleton[i].data.eSkeletonPositionTrackingState[joint.pos1] == NUI_SKELETON_POSITION_TRACKED || skeleton[i].data.eSkeletonPositionTrackingState[joint.pos2] == NUI_SKELETON_POSITION_TRACKED)
                 painter.setPen(QPen(Qt::green, 1));
             else
                 painter.setPen(QPen(Qt::darkGray, 1));
 
+            // Point de chaque extremitée
             painter.drawEllipse(pos1, 2, 2);
             painter.drawEllipse(pos2, 2, 2);
         }
 
+        // For fun : On grossi les point de la tête et des main pour les mettre en valeur
         QPoint headPos = adjustSize(skeleton[i].data.SkeletonPositions[NUI_SKELETON_POSITION_HEAD]);
         QPoint leftPos = adjustSize(skeleton[i].data.SkeletonPositions[NUI_SKELETON_POSITION_HAND_LEFT]);
         QPoint rightPos = adjustSize(skeleton[i].data.SkeletonPositions[NUI_SKELETON_POSITION_HAND_RIGHT]);
@@ -108,7 +129,7 @@ void DrawZone::paintEvent(QPaintEvent*)
 
 void DrawZone::mousePressEvent(QMouseEvent* e)
 {
-    QRect rect = zones[current].rect;
+    /*QRect rect = zones[current].rect;
 
     if(rect.x() == -1)
     {
@@ -120,39 +141,40 @@ void DrawZone::mousePressEvent(QMouseEvent* e)
         QPoint point = lissage(e->pos().x(), e->pos().y());
         zones[current].rect = QRect(rect.x(), rect.y(), point.x() - rect.x(), point.y() - rect.y());
         QCoreApplication::postEvent(mMain, new CustomEvent<SelectZoneEvent>());
-    }
+    }*/
 }
 
 bool DrawZone::addZone(std::string name)
 {
-    t_zone zone = {true, QRect(-1, -1, -1, -1)};
+    /*t_zone zone = {true, QRect(-1, -1, -1, -1)};
     zones[name] = zone;
     current = name;
-    return true;
+    return true;*/
 }
 
 void DrawZone::hightlight(QPainter& painter, int posX, int posY)
 {
-    for(int x = 0; x <= this->width(); x += CASE_SIZE)
+    /*for(int x = 0; x <= this->width(); x += CASE_SIZE)
     {
         if(posX >= x && posX < x + CASE_SIZE)
         {
             for(int y = 0; y <= this->height(); y += CASE_SIZE)
             {
                 if(posY >= y && posY < y + CASE_SIZE)
-                {
+                {*/
                     painter.setPen(QPen(Qt::blue, 2));
                     painter.setBrush(Qt::white);
-                    painter.drawRect(QRect(x, y, CASE_SIZE, CASE_SIZE));
-                }
+                    //painter.drawRect(QRect(x, y, CASE_SIZE, CASE_SIZE));
+                    painter.drawRect(QRect(posX - 5, posY - 5, CASE_SIZE, CASE_SIZE));
+                /*}
             }
         }
-    }
+    }*/
 }
 
 QPoint DrawZone::lissage(int posX, int posY)
 {
-    QPoint point(-1, -1);
+    /*QPoint point(-1, -1);
 
     for(int x = 0; x <= this->width(); x += CASE_SIZE)
     {
@@ -170,9 +192,14 @@ QPoint DrawZone::lissage(int posX, int posY)
         }
     }
 
-    return point;
+    return point;*/
 }
 
+/**
+ * @brief DrawZone::adjustSize : ajuste la position des point en fonction de la taille de la zone de dessin
+ * @param skeletonPoint : point d'origine
+ * @return : point ajustée
+ */
 QPoint DrawZone::adjustSize(Vector4 skeletonPoint)
 {
     LONG x, y;
@@ -186,6 +213,11 @@ QPoint DrawZone::adjustSize(Vector4 skeletonPoint)
     return QPoint(pointX, pointY);
 }
 
+/**
+ * @brief DrawZone::SetSkeleton : enregistre les coordonnées de chauqe points du squelette
+ * @param id : numéro du squelette
+ * @param data : coordonnées de chaque points
+ */
 void DrawZone::SetSkeleton(int id, NUI_SKELETON_DATA data)
 {
     if(id >= 5)
@@ -194,6 +226,13 @@ void DrawZone::SetSkeleton(int id, NUI_SKELETON_DATA data)
     skeleton[id].data = data;
 }
 
+/**
+ * @brief DrawZone::DrawBone : enregistre les jointure a dessiner (+ indique que le squelette est toujours traqué)
+ * @param id : numéro du squelette
+ * @param joint : numéro de la jointure
+ * @param pos1 : position d'un extremitée
+ * @param pos2 : position de l'autre extremité
+ */
 void DrawZone::DrawBone(int id, int joint, NUI_SKELETON_POSITION_INDEX pos1, NUI_SKELETON_POSITION_INDEX pos2)
 {
     if(id >= 5)
@@ -206,6 +245,10 @@ void DrawZone::DrawBone(int id, int joint, NUI_SKELETON_POSITION_INDEX pos1, NUI
     skeleton[id].untracked = 0;
 }
 
+/**
+ * @brief DrawZone::RemoveSkeleton : supprime une squelette du dessin
+ * @param id : numéro du squelette
+ */
 void DrawZone::RemoveSkeleton(int id)
 {
     if(id >= 5)
@@ -214,11 +257,17 @@ void DrawZone::RemoveSkeleton(int id)
     skeleton[id].untracked++;
 }
 
+/**
+ * @brief DrawZone::lock : vérouille le mutex du vecteur de données des squelettes
+ */
 void DrawZone::lock()
 {
     mutex.lock();
 }
 
+/**
+ * @brief DrawZone::unlock : dévérouille le mutex du vecteur de données des squelettes
+ */
 void DrawZone::unlock()
 {
     mutex.unlock();
--- qkinect.cpp
+++ qkinect.cpp
@@ -2,6 +2,11 @@
 #include <QDebug>
 #include <exception>
 
+/**
+ * @brief QKinect::QKinect : constructeur
+ * @param parent : classe parent
+ * @param drawZone : zone de dessin
+ */
 QKinect::QKinect(QObject *parent, DrawZone* drawZone) :
     QThread(parent),
     isRunning(false),
@@ -10,6 +15,9 @@ QKinect::QKinect(QObject *parent, DrawZone* drawZone) :
 {
 }
 
+/**
+ * @brief QKinect::start : démarrage de la capture de données
+ */
 void QKinect::start()
 {
     if(isRunning)
@@ -19,6 +27,9 @@ void QKinect::start()
     QThread::start();
 }
 
+/**
+ * @brief QKinect::stop : arrêt de la capture de données
+ */
 void QKinect::stop()
 {
     if(isRunning)
@@ -33,13 +44,18 @@ void QKinect::stop()
     isInitialized = false;
 }
 
+/**
+ * @brief QKinect::run : fonction executer par QThread
+ */
 void QKinect::run()
 {
     try
     {
+        // Initialisation de la Kinect
         qDebug() << "initialization...";
         init();
 
+        // Boucle de capture des données
         qDebug() << "tracking...";
         tracking();
     }
@@ -49,8 +65,12 @@ void QKinect::run()
     }
 }
 
+/**
+ * @brief QKinect::init : initialisation de la Kinect
+ */
 void QKinect::init()
 {
+    // Initialisation de capteur squeletique
     HRESULT res = NuiInitialize(NUI_INITIALIZE_FLAG_USES_SKELETON);
 
     if(res != S_OK)
@@ -58,6 +78,7 @@ void QKinect::init()
         throw std::logic_error("No Kinect found...");
     }
 
+    // Démarrage de la capture
     res = NuiSkeletonTrackingEnable(NULL, 0);
 
     if(res != S_OK)
@@ -67,10 +88,15 @@ void QKinect::init()
     isRunning = true;
 }
 
+/**
+ * @brief QKinect::tracking : Boucle principale de capture
+ */
 void QKinect::tracking()
 {
+    // Tant que la capture est demandé
     while(isRunning)
     {
+        // Récupération de la frame suivante
         HRESULT res = NuiSkeletonGetNextFrame(1, &mSkeletonFrame);
 
         NuiTransformSmooth(&mSkeletonFrame, NULL);
@@ -79,11 +105,13 @@ void QKinect::tracking()
         {
             if(mSkeletonFrame.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED)
             {
+                // vérouillage du mutex du vecteur des point du squelette
                 mDrawZone->lock();
 
+                // Enregistrement des coordonnée de chaque points pour la zone de dessin
                 mDrawZone->SetSkeleton(i, mSkeletonFrame.SkeletonData[i]);
 
-                // Render Torso
+                // Jointure du torse
                 mDrawZone->DrawBone(i, 0, NUI_SKELETON_POSITION_HEAD, NUI_SKELETON_POSITION_SHOULDER_CENTER);
                 mDrawZone->DrawBone(i, 1, NUI_SKELETON_POSITION_SHOULDER_CENTER, NUI_SKELETON_POSITION_SHOULDER_LEFT);
                 mDrawZone->DrawBone(i, 2, NUI_SKELETON_POSITION_SHOULDER_CENTER, NUI_SKELETON_POSITION_SHOULDER_RIGHT);
@@ -92,34 +120,37 @@ void QKinect::tracking()
                 mDrawZone->DrawBone(i, 5, NUI_SKELETON_POSITION_HIP_CENTER, NUI_SKELETON_POSITION_HIP_LEFT);
                 mDrawZone->DrawBone(i, 6, NUI_SKELETON_POSITION_HIP_CENTER, NUI_SKELETON_POSITION_HIP_RIGHT);
 
-                // Left Arm
+                // Joiunture bras gauche
                 mDrawZone->DrawBone(i, 7, NUI_SKELETON_POSITION_SHOULDER_LEFT, NUI_SKELETON_POSITION_ELBOW_LEFT);
                 mDrawZone->DrawBone(i, 8, NUI_SKELETON_POSITION_ELBOW_LEFT, NUI_SKELETON_POSITION_WRIST_LEFT);
                 mDrawZone->DrawBone(i, 9, NUI_SKELETON_POSITION_WRIST_LEFT, NUI_SKELETON_POSITION_HAND_LEFT);
 
-                // Right Arm
+                // Jointure bras droit
                 mDrawZone->DrawBone(i, 10, NUI_SKELETON_POSITION_SHOULDER_RIGHT, NUI_SKELETON_POSITION_ELBOW_RIGHT);
                 mDrawZone->DrawBone(i, 11, NUI_SKELETON_POSITION_ELBOW_RIGHT, NUI_SKELETON_POSITION_WRIST_RIGHT);
                 mDrawZone->DrawBone(i, 12, NUI_SKELETON_POSITION_WRIST_RIGHT, NUI_SKELETON_POSITION_HAND_RIGHT);
 
-                // Left Leg
+                // Jointure jambe gauche
                 mDrawZone->DrawBone(i, 13, NUI_SKELETON_POSITION_HIP_LEFT, NUI_SKELETON_POSITION_KNEE_LEFT);
                 mDrawZone->DrawBone(i, 14, NUI_SKELETON_POSITION_KNEE_LEFT, NUI_SKELETON_POSITION_ANKLE_LEFT);
                 mDrawZone->DrawBone(i, 15, NUI_SKELETON_POSITION_ANKLE_LEFT, NUI_SKELETON_POSITION_FOOT_LEFT);
 
-                // Right Leg
+                // Jointure jambe droite
                 mDrawZone->DrawBone(i, 16, NUI_SKELETON_POSITION_HIP_RIGHT, NUI_SKELETON_POSITION_KNEE_RIGHT);
                 mDrawZone->DrawBone(i, 17, NUI_SKELETON_POSITION_KNEE_RIGHT, NUI_SKELETON_POSITION_ANKLE_RIGHT);
                 mDrawZone->DrawBone(i, 18, NUI_SKELETON_POSITION_ANKLE_RIGHT, NUI_SKELETON_POSITION_FOOT_RIGHT);
 
+                // dévérouillage du mutex du vecteur des point du squelette
                 mDrawZone->unlock();
             }
             else if(mSkeletonFrame.SkeletonData[i].eTrackingState == NUI_SKELETON_NOT_TRACKED)
             {
+                // Squelette n'est plus traquer, on le retire de la zone de dessin
                 mDrawZone->RemoveSkeleton(i);
             }
         }
 
+        // sleep de 30ms pour éviter de pourrir la ressource CPU :)
         msleep(30);
     }
 }
